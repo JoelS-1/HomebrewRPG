04f33b5217daccc1dc6f3d6bfdce4d5eadcc1e09
JoelS-1 <82602257+JoelS-1@users.noreply.github.com>
2021-06-28 14:32:38 -0400

    Add project files.

---
+++ HomebrewRPG.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HomebrewRPG.WebMVC.MvcApplication" Language="C#" %>
---
+++ HomebrewRPG.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomebrewRPG.WebMVC.Startup))]
+namespace HomebrewRPG.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
