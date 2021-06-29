1af94edb6f75ba7fd5c05f87fdd221cce628962d
JoelS-1 <82602257+JoelS-1@users.noreply.github.com>
2021-06-29 02:24:54 -0400

    added character data table, controller, and view

---
+++ HomebrewRPG.Data/Character.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomebrewRPG.Data
+{
+    public class Character
+    {
+        [Key]
+        public int CharacterId { get; set; }
+        [Required]
+        public string CharacterName { get; set; }
+        [Required]
+        public string Race { get; set; }
+        [Required]
+        public int CharacterLevel { get; set; }
+
+        [Required]
+        public int Health { get; set; }
+        [Required]
+        public int Strength { get; set; }
+        [Required]
+        public int Agility { get; set; }
+        [Required]
+        public int Intelligence { get; set; }
+        [Required]
+        public int Charisma { get; set; }
+
+        public int HitPoints { get; set; }
+        public int Sanity { get; set; }
+        public int Dodge { get; set; }
+        public int Reaction { get; set; }
+        public int BaseProwess { get; set; }
+        public int Magic { get; set; }
+        public int Fate { get; set; }
+        public int Speed { get; set; }
+
+
+        public int DamageResistance { get; set; }
+        public int MagicResistance { get; set; }
+
+        public Guid OwnerId { get; set; }
+    }
+}
---
+++ HomebrewRPG.Models/CharacterListItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomebrewRPG.Models
+{
+    public class CharacterListItem
+    {
+        public int CharacterId { get; set; }
+        public string CharacterName { get; set; }
+        public string Race { get; set; }
+        public int CharacterLevel { get; set; }
+    }
+}
