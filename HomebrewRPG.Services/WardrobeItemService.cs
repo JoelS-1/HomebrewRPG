6ef3efaae8fb4aea1330364999ddcd60462d9e68
JoelS-1 <82602257+JoelS-1@users.noreply.github.com>
2021-07-02 16:03:52 -0400

    wardrobe item create and list

---
+++ HomebrewRPG.Services/WardrobeItemService.cs
@@ -0,0 +1,63 @@
+﻿using HomebrewRPG.Data;
+using HomebrewRPG.Models.WardrobeItemModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomebrewRPG.Services
+{
+    public class WardrobeItemService
+    {
+        ApplicationDbContext _ctx = new ApplicationDbContext();
+
+        private readonly Guid _userId;
+        public WardrobeItemService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateWardrobeItem(WardrobeItemCreate model)
+        {
+            var entity =
+                new WardrobeItem()
+                {
+                    OwnerId = _userId,
+                    ArmorName = model.ArmorName,
+                    ArmorType = model.ArmorType,
+                    Description = model.Description,
+                    HealthRequired = model.HealthRequired,
+                    StrengthRequired = model.StrengthRequired,
+                    AgilityRequired = model.AgilityRequired,
+                    MagicRequired = model.MagicRequired,
+                    Special = model.Special,
+                    PhysicalBlocking = model.PhysicalBlocking,
+                    MagicalBlocking = model.MagicalBlocking,
+                    PhysicalResistance = model.PhysicalResistance,
+                    MagicalResistance = model.MagicalResistance
+                };
+            _ctx.WardrobeItems.Add(entity);
+            return _ctx.SaveChanges() == 1;
+        }
+
+        public IEnumerable<WardrobeListItem> GetWardrobeListItems()
+        {
+            var query =
+                _ctx
+                    .WardrobeItems
+                    .Where(e => e.OwnerId == _userId)
+                    .Select(
+                        e =>
+                            new WardrobeListItem
+                            {
+                                WardobeItemId = e.WardobeItemId,
+                                ArmorName = e.ArmorName,
+                                ArmorType = e.ArmorType,
+                                Description = e.Description
+                            }
+                    );
+            return query.ToArray();
+        }
+    }
+}
---
+++ HomebrewRPG.WebMVC/Controllers/WardrobeItemController.cs
@@ -0,0 +1,53 @@
+﻿using HomebrewRPG.Models.WardrobeItemModels;
+using HomebrewRPG.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomebrewRPG.WebMVC.Controllers
+{
+    public class WardrobeItemController : Controller
+    {
+        // GET: WardrobeItem
+        public ActionResult Index()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new WardrobeItemService(userId);
+            var model = service.GetWardrobeListItems();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(WardrobeItemCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var service = CreateWardrobeItemService();
+            if (service.CreateWardrobeItem(model))
+            {
+                TempData["SaveResult"] = "Your wardrobe item was created.";
+                return RedirectToAction("Index");
+            };
+
+            ModelState.AddModelError("", "Wardrobe item could not be created.");
+
+            return View("Index");
+        }
+
+        private WardrobeItemService CreateWardrobeItemService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new WardrobeItemService(userId);
+            return service;
+        }
+    }
+}
