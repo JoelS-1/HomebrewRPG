bf22bf790874e4e7c1219bf78f0104eabaa1ec34
JoelS-1 <82602257+JoelS-1@users.noreply.github.com>
2021-07-02 16:43:23 -0400

    spell create and list

---
+++ HomebrewRPG.Services/SpellService.cs
@@ -0,0 +1,56 @@
+﻿using HomebrewRPG.Data;
+using HomebrewRPG.Models.SpellModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomebrewRPG.Services
+{
+    public class SpellService
+    {
+        ApplicationDbContext _ctx = new ApplicationDbContext();
+
+        private readonly Guid _userId;
+        public SpellService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateSpell(SpellCreate model)
+        {
+            var entity =
+                new Spell()
+                {
+                    OwnerId = _userId,
+                    SpellName = model.SpellName,
+                    SpellDescription = model.SpellDescription,
+                    SpellEffect = model.SpellEffect,
+                    SpellType = model.SpellType,
+                    Range = model.Range,
+                    SpellDC = model.SpellDC
+                };
+            _ctx.Spells.Add(entity);
+            return _ctx.SaveChanges() == 1;
+        }
+
+        public IEnumerable<SpellListItem> GetSpells()
+        {
+            var query =
+                _ctx
+                    .Spells
+                    .Where(e => e.OwnerId == _userId)
+                    .Select(
+                        e =>
+                            new SpellListItem
+                            {
+                                SpellId = e.SpellId,
+                                SpellName = e.SpellName,
+                                SpellType = e.SpellType
+                            }
+                    );
+            return query.ToArray();
+        }
+    }
+}
---
+++ HomebrewRPG.WebMVC/Controllers/SpellController.cs
@@ -0,0 +1,53 @@
+﻿using HomebrewRPG.Models.SpellModels;
+using HomebrewRPG.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomebrewRPG.WebMVC.Controllers
+{
+    public class SpellController : Controller
+    {
+        // GET: Spell
+        public ActionResult Index()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new SpellService(userId);
+            var model = service.GetSpells();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(SpellCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var service = CreateSpellService();
+            if (service.CreateSpell(model))
+            {
+                TempData["SaveResult"] = "Your spell was created.";
+                return RedirectToAction("Index");
+            };
+
+            ModelState.AddModelError("", "Your spell could not be created.");
+
+            return View("Index");
+        }
+
+        private SpellService CreateSpellService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new SpellService(userId);
+            return service;
+        }
+    }
+}
