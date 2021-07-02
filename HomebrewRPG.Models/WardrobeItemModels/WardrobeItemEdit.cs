11609398859e1330e6fea191fe3e44e35bd67f29
JoelS-1 <82602257+JoelS-1@users.noreply.github.com>
2021-07-02 15:50:17 -0400

    wardrobe item models

---
+++ HomebrewRPG.Models/WardrobeItemModels/WardrobeItemEdit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomebrewRPG.Models.WardrobeItemModels
+{
+    public class WardrobeItemEdit
+    {
+        public int WardobeItemId { get; set; }
+        public string ArmorName { get; set; }
+        public string ArmorType { get; set; }
+        public string Description { get; set; }
+
+        public int HealthRequired { get; set; }
+        public int StrengthRequired { get; set; }
+        public int AgilityRequired { get; set; }
+        public int MagicRequired { get; set; }
+        public string Special { get; set; }
+
+        public int PhysicalBlocking { get; set; }
+        public int MagicalBlocking { get; set; }
+
+        public int PhysicalResistance { get; set; }
+        public int MagicalResistance { get; set; }
+    }
+}
