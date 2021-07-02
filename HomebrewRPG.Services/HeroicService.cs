ef2e23f2312c2508ddec6f654eb49c570715d67e
JoelS-1 <82602257+JoelS-1@users.noreply.github.com>
2021-07-02 17:29:44 -0400

    heroic list and create

---
+++ HomebrewRPG.Services/HeroicService.cs
@@ -0,0 +1,63 @@
+﻿using HomebrewRPG.Data;
+using HomebrewRPG.Models.HeroicModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomebrewRPG.Services
+{
+    public class HeroicService
+    {
+        ApplicationDbContext _ctx = new ApplicationDbContext();
+
+        private readonly Guid _userId;
+        public HeroicService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateHeroic(HeroicCreate model)
+        {
+            var entity =
+                new Heroic()
+                {
+                    OwnerId = _userId,
+                    HeroicName = model.HeroicName,
+                    IsPersonalHeroic = model.IsPersonalHeroic,
+                    Description = model.Description,
+                    RequiredLevel = model.RequiredLevel,
+                    RequiredHeroic = model.RequiredHeroic,
+
+                    RequiredAgility = model.RequiredAgility,
+                    RequiredStrength = model.RequiredStrength,
+                    RequiredInstinct = model.RequiredInstinct,
+                    RequiredIntelligence = model.RequiredIntelligence,
+                    RequiredCharisma = model.RequiredCharisma,
+                    RequiredHealth = model.RequiredHealth
+                };
+            _ctx.Heroics.Add(entity);
+            return _ctx.SaveChanges() == 1;
+        }
+
+        public IEnumerable<HeroicListItem> GetHeroics()
+        {
+            var query =
+                _ctx
+                    .Heroics
+                    .Where(e => e.OwnerId == _userId)
+                    .Select(
+                        e =>
+                            new HeroicListItem
+                            {
+                                HeroicId = e.HeroicId,
+                                HeroicName = e.HeroicName,
+                                Description = e.Description,
+                                IsPersonalHeroic = e.IsPersonalHeroic
+                            }
+                    );
+            return query.ToArray();
+        }
+    }
+}
---
+++ HomebrewRPG.WebMVC/Controllers/HeroicController.cs
@@ -0,0 +1,53 @@
+﻿using HomebrewRPG.Models.HeroicModels;
+using HomebrewRPG.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomebrewRPG.WebMVC.Controllers
+{
+    public class HeroicController : Controller
+    {
+        // GET: Heroic
+        public ActionResult Index()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new HeroicService(userId);
+            var model = service.GetHeroics();
+            return View(model);
+        }
+
+        private HeroicService CreateHeroicService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new HeroicService(userId);
+            return service;
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(HeroicCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var service = CreateHeroicService();
+            if (service.CreateHeroic(model))
+            {
+                TempData["SaveResult"] = "Your heroic was created.";
+                return RedirectToAction("Index");
+            };
+
+            ModelState.AddModelError("", "Your heroic could not be created.");
+
+            return View("Index");
+        }
+    }
+}
