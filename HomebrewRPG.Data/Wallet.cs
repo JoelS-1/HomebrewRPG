3b923770f22461d428553a2158d841ae3775c6cd
JoelS-1 <82602257+JoelS-1@users.noreply.github.com>
2021-07-13 22:41:59 -0400

    wallet class and models

--- HomebrewRPG.Data/Wallet.cs
+++ HomebrewRPG.Data/Wallet.cs
@@ -1,24 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HomebrewRPG.Data
 {
-    public class Money
+    public class Wallet
     {
-        [Key]
-        public int MoneyId { get; set; }
+        public int WalletId { get; set; }
         public int Gold { get; set; }
         public int Silver { get; set; }
         public int Copper { get; set; }
-
-        [ForeignKey(nameof(Character))]
-        public int CharacterId { get; set; }
-        public virtual Character Character { get; set; }
         public Guid OwnerId { get; set; }
     }
 }
---
+++ HomebrewRPG.Models/WalletModels/WalletEdit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomebrewRPG.Models.WalletModels
+{
+    public class WalletEdit
+    {
+        public int WalletId { get; set; }
+        public int Gold { get; set; }
+        public int Silver { get; set; }
+        public int Copper { get; set; }
+    }
+}
